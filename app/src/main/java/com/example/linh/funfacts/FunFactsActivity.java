4d57ad0064da7c5e90b245d0707ec4f79e90071a
Linh1991 <mr.yagamiraito@gmail.com>
2016-04-21 23:03:37 +0700

    seccon commit

---
+++ app/src/main/java/com/example/linh/funfacts/FunFactsActivity.java
@@ -0,0 +1,34 @@
+package com.example.linh.funfacts;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+        Button ShowFact = (Button) findViewById(R.id.ShowFactsButton);
+        TextView factLabel = (TextView) findViewById(R.id.factLabel);
+        ShowFact.setOnClickListener();
+
+    }
+
+
+    }
+}
