dddd1bb136113a5c01e0a60e7ed4c724de8629e6
Linh1991 <mr.yagamiraito@gmail.com>
2016-04-22 11:11:15 +0700

    Array facts

--- app/src/main/java/com/example/linh/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/linh/funfacts/FunFactsActivity.java
@@ -32,18 +32,21 @@ public class FunFactsActivity extends AppCompatActivity {
             @Override
             public void onClick(View View){
                 String fact = " ";
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
                 Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-               if(randomNumber == 0)
-               {
-                   fact = "Pig can fly";
-               }else if (randomNumber == 1)
-               {
-                   fact = "Ants strect when they wake up in the morning";
-               }else if (randomNumber == 2)
-               {
-                   fact = "Dog can say meo meo";
-               }
+                int randomNumber = randomGenerator.nextInt(9);
+
+                fact = facts[randomNumber];
                 factLabel.setText(fact);
 
 
